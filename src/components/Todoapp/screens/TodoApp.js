56ecebf18de94977ed39c57c32403839ac073d5c
Varshavt1999 <varshavt7399@gmail.com>
2022-03-07 18:08:59 +0530

    todo app styling compleated

--- src/components/Todoapp/screens/TodoApp.js
+++ src/components/Todoapp/screens/TodoApp.js
@@ -10,11 +10,11 @@ export default class TodoApp extends Component {
             <TodoContainer>
                 <Title>Todo App</Title>
                 <Form>
-                    <Input type="text" placeholder='Enter items'/>
+                    <Input type="text" placeholder='Enter items...'/>
                     {/* <Button>Add</Button> */}
                 </Form>
                 <ItemSection>
-                    <Item>item1</Item>
+                    <Item><Name>item1 </Name><Icon className="fa-solid fa-trash-can"></Icon></Item>
                 </ItemSection>
             </TodoContainer>
         </OuterContainer>
@@ -29,7 +29,7 @@ const OuterContainer = styled.div`
     height:100vh ;
 `;
 const TodoContainer = styled.div`
-    background-color:#000 ;
+    background-color:#056679 ;
     width:50% ;
     margin:0 auto ;
     padding:20px ;
@@ -40,26 +40,51 @@ const Title = styled.h2`
     color:#fff ;
 `;
 const Form = styled.form`
-    background-color:#fff ;
-    padding:20px ;
-    margin-bottom:20px ;
-    border-radius:4px;
+    width:100% ;
+    margin-bottom:20px ; 
 `;
 const Input = styled.input`
-    width:90%;
-    margin:0 auto ;
-    padding:10px ;
+    background-color:#056679 ;
+    padding:15px ;
+    width:100% ;
+    border-radius:4px;
+    border:1px solid #fff ;
+    color: #a9a9a9;
+    font-weight:600 ;
+    outline:none ;
+    ::placeholder {
+       color: #a9a9a9;
+       font-weight:600 ;
+   }
 `;
 // const Button = styled.button`
 
 // `;
 const ItemSection = styled.ul`
-    background-color:#fff ;
-    padding:20px ;
+    /* background-color:#fff ; */
+    /* padding-top:20px; */
     border-radius:4px;
     min-height:150px ;
+    
 `;
 const Item = styled.li`
-
+    text-align:left ;
+    padding:10px 20px;
+    background-color:#056679;
+    margin-bottom:6px ;
+    border-radius:4px ;
+    box-shadow: 1px 3px 4px 0px #000;
+    display:flex ;
+    justify-content: space-between;
+`;
+const Name = styled.h5`
+    font-weight:700 ;
+    font-size:16px ;
+    color:#cdcdcd ;
+`;
+const Icon = styled.div`
+    font-weight:700 ;
+    font-size:16px ;
+    color:#cdcdcd ;
 `;
 
